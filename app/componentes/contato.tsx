350ba4640b2aec67f38ba3fec5164539ede71b32
Pedro Afonso <157052926+pedroafonso18@users.noreply.github.com>
2024-04-01 21:07:50 -0300

    Update contato.tsx

--- app/componentes/contato.tsx
+++ app/componentes/contato.tsx
@@ -2,10 +2,10 @@ export default function Contato(){
     return(
 
  <div className="grid grid-cols-7 pl-40 ml-px size-30 mt-2">      
-    <a href="https://github.com/picola18">
+    <a href="https://github.com/pedroafonso18">
         <img src="/icons/github-line.svg"/>
     </a>
-    <a href="https://twitter.com/panetonedemijo">
+    <a href="https://twitter.com/pedroafonso_05">
         <img src="/icons/twitter-x-line.svg"/>
     </a>
 </div>
